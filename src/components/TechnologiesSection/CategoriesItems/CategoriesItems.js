a881fef852b318be559e6b0e99e5cb8bed0e503c
Anwer Baccar <anwer.baccar.pro@gmail.com>
2021-10-07 21:34:46 +0100

    select by category finished

--- src/components/TechnologiesSection/CategoriesItems/CategoriesItems.js
+++ src/components/TechnologiesSection/CategoriesItems/CategoriesItems.js
@@ -2,13 +2,13 @@ import React from "react";
 import classes from "./CategoriesItems.module.css";
 import CategoriesItem from "./CategoriesItem/CategoriesItem";
 
-const CategoriesItems = () => {
+const CategoriesItems = (props) => {
   const list = ["All", "Web", "Mobile", "AR", "Other"];
 
   return (
     <ul className={classes.CategoriesItems}>
       {list.map((item, index) => {
-        return <CategoriesItem key={index}>{item}</CategoriesItem>;
+        return <CategoriesItem key={index}><button onClick={() => {return (props.handleSetTag(item))}}>{item}</button></CategoriesItem>;
       })}
     </ul>
   );
