9f7121f17ae4e30f607f13591ecf3ebafaedc9d0
Anwer Baccar <anwer.baccar.pro@gmail.com>
2021-10-09 15:02:20 +0100

    adding http get req to firebase and load about section data and services data from firebase

--- src/components/Sections/AboutMeSection/AboutMeSection.js
+++ src/components/Sections/AboutMeSection/AboutMeSection.js
@@ -1,29 +1,40 @@
-import Divider from "../../UI/Divider/Divider";
-import React from "react";
-import classes from "./AboutMeSection.module.css";
-import "../grid.css";
-import anwerImg from "../../../assets/anwer2.jpg";
-import RoundedButton from "../../UI/RoundedButton/RoundedButton";
+import Divider from '../../UI/Divider/Divider';
+import React, { useEffect, useState } from 'react';
+import classes from './AboutMeSection.module.css';
+import '../grid.css';
+import anwerImg from '../../../assets/anwer2.jpg';
+import RoundedButton from '../../UI/RoundedButton/RoundedButton';
 
 const AboutMeSection = () => {
+
+  const [description,setDescription] = useState();
+  const [imageUrl,setImageUrl] = useState();
+
+  useEffect(() => {
+    fetch(
+      `${process.env.REACT_APP_FIREBASE_URL}/aboutSection.json`,
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        setDescription(responseData.description);
+        setImageUrl(responseData.imageUrl)
+      });
+  }, [description]);
+
   return (
     <section>
       <Divider title="About Me" />
       <div className="row">
         <div className="col span-1-of-2 box">
-          <img src={anwerImg} alt="anwer" className={classes.anwer} />
+          <img src={imageUrl} alt="anwer" className={classes.anwer} />
         </div>
         <div className="col span-1-of-2 box">
-          <div className={"row span-1-of-3 box " + classes.whoAmISection}>
+          <div className={'row span-1-of-3 box ' + classes.whoAmISection}>
             <h1 className={classes.whoAmI}>Who AM I</h1>
           </div>
 
           <p className={classes.description}>
-            Le lorem ipsum est, en imprimerie, une suite de mots sans
-            signification utilisée à titre provisoire pour calibrer une mise en
-            page, le texte définitif venant remplacer le faux-texte dès qu'il
-            est prêt ou que la mise en page est achevée. Généralement, on
-            utilise un texte en faux latin, le Lorem ipsum ou Lipsum.
+            {description}
           </p>
 
           <div className="row span-1-of-3 box">
--- src/components/Sections/ServicesSection/Services.js
+++ src/components/Sections/ServicesSection/Services.js
@@ -1,47 +1,31 @@
-import React from "react";
-import classes from "./Services.module.css";
-import "../grid.css";
-import Divider from "../../UI/Divider/Divider";
-import ServicesCard from "../../ServicesSectionComponent/Card/ServicesCard";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import settings from "../../../utils/TechnologiesSettings/TechnologiesSettings"
+import React, { useState, useEffect } from 'react';
+import classes from './Services.module.css';
+import '../grid.css';
+import Divider from '../../UI/Divider/Divider';
+import ServicesCard from '../../ServicesSectionComponent/Card/ServicesCard';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import settings from '../../../utils/TechnologiesSettings/TechnologiesSettings';
 
 const Services = () => {
+  const [list, setList] = useState([]);
 
-  const list = [
-    {
-      name: "AR Development",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/ar.svg?alt=media&token=8024cac5-7d71-4b7d-b6d8-9689241c3b7f",
-    },
-    {
-      name: "Game Development",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/ar.svg?alt=media&token=8024cac5-7d71-4b7d-b6d8-9689241c3b7f",
-    },
-    {
-      name: "Mobile Development",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/ar.svg?alt=media&token=8024cac5-7d71-4b7d-b6d8-9689241c3b7f",
-    },
-    {
-      name: "Web Development",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/ar.svg?alt=media&token=8024cac5-7d71-4b7d-b6d8-9689241c3b7f",
-    },
-    {
-      name: "Video Editing",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/ar.svg?alt=media&token=8024cac5-7d71-4b7d-b6d8-9689241c3b7f",
-    },
-    {
-      name: "Web Design",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/ar.svg?alt=media&token=8024cac5-7d71-4b7d-b6d8-9689241c3b7f",
-    },
-  ];
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_FIREBASE_URL}/servicesSection.json`)
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedServicesList = [];
+        for (const key in responseData) {
+          loadedServicesList.push({
+            id: key,
+            name: responseData[key].name,
+            image: responseData[key].image,
+          });
+        }
+        setList(loadedServicesList);
+      });
+  }, [list]);
 
   return (
     <section>
@@ -50,7 +34,7 @@ const Services = () => {
         <Slider {...settings}>
           {list.map((item, index) => {
             return (
-              <div style={{ color: "red" }} key={index}>
+              <div style={{ color: 'red' }} key={index}>
                 <ServicesCard name={item.name} image={item.image} />
               </div>
             );
