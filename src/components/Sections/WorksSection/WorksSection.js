55d217734e14c88dcf01908c6262487b9030fb5a
Anwer Baccar <anwer.baccar.pro@gmail.com>
2021-10-09 15:26:02 +0100

    adding http to works and technologies sections

--- src/components/Sections/WorksSection/WorksSection.js
+++ src/components/Sections/WorksSection/WorksSection.js
@@ -1,86 +1,59 @@
-import React, { useEffect, useState } from "react";
-import classes from "./WorksSection.module.css";
-import Divider from "../../UI/Divider/Divider";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import settings from "../../../utils/WorksSettings/WorksSettings";
-import CategoriesItems from "../../UI/CategoriesItems/CategoriesItems";
-import WorksCard from "../../WorksSectionComponent/WorksCard/WorksCard";
+import React, { useEffect, useState } from 'react';
+import classes from './WorksSection.module.css';
+import Divider from '../../UI/Divider/Divider';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import settings from '../../../utils/WorksSettings/WorksSettings';
+import CategoriesItems from '../../UI/CategoriesItems/CategoriesItems';
+import WorksCard from '../../WorksSectionComponent/WorksCard/WorksCard';
 
 const WorksSection = () => {
-  const list = [
-    {
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/download.jpg?alt=media&token=6403af6d-8b59-42d0-aa49-6a45b61c8642",
-      name: "Work Example",
-      description:
-        "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.",
-      category: "Web",
-      url: "https://play.google.com/store/apps/details?id=com.facebook.katana",
-    },
-    {
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/download.jpg?alt=media&token=6403af6d-8b59-42d0-aa49-6a45b61c8642",
-      name: "Work Example",
-      description:
-        "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.",
-      category: "Mobile",
-      url: "https://www.jumia.com.tn/",
-    },
-    {
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/download.jpg?alt=media&token=6403af6d-8b59-42d0-aa49-6a45b61c8642",
-      name: "Work Example",
-      description:
-        "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.",
-      category: "AR",
-    },
-    {
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/download.jpg?alt=media&token=6403af6d-8b59-42d0-aa49-6a45b61c8642",
-      name: "Work Example",
-      description:
-        "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.",
-      category: "Mobile",
-    },
-    {
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/download.jpg?alt=media&token=6403af6d-8b59-42d0-aa49-6a45b61c8642",
-      name: "Work Example",
-      description:
-        "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.",
-      category: "Mobile",
-    },
-    {
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/decori-38bc0.appspot.com/o/download.jpg?alt=media&token=6403af6d-8b59-42d0-aa49-6a45b61c8642",
-      name: "Work Example",
-      description:
-        "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.",
-      category: "Mobile",
-    },
-  ];
+  const [list, setList] = useState([]);
 
-  const [category, setCategory] = useState("All");
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_FIREBASE_URL}/worksSection.json`)
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedServicesList = [];
+        for (const key in responseData) {
+          loadedServicesList.push({
+            id: key,
+            name: responseData[key].name,
+            image: responseData[key].image,
+            description: responseData[key].description,
+            category: responseData[key].category,
+            url: responseData[key].url,
+          });
+        }
+        setList(loadedServicesList);
+      });
+  }, [list]);
+
+  const [category, setCategory] = useState('All');
   const [filteredList, setFiltredList] = useState([]);
 
   useEffect(() => {
-    category === "All"
+    category === 'All'
       ? setFiltredList(list)
       : setFiltredList(list.filter((item) => item.category === category));
-  }, [category]);
+  }, [category, list]);
 
   const rendredComponent =
     filteredList.length === 0 ? (
-      <h3 style={{ textAlign: "center" }}>No DATA!</h3>
+      <h3 style={{ textAlign: 'center' }}>No DATA!</h3>
     ) : (
       <div>
         <Slider {...settings}>
           {filteredList.map((item, index) => {
             return (
-              <div className={classes.dd} key={index}>
-                <WorksCard name={item.name} image={item.image} description={item.description} url={item.url} />
+              <div className={classes.dd} key={item.id}>
+                <WorksCard
+                  name={item.name}
+                  image={item.image}
+                  description={item.description}
+                  url={item.url}
+                />
               </div>
             );
           })}
