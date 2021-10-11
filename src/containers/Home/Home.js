7a75ab09a9fb1e46a4f7b3be260b426d4c2db4ee
Anwer Baccar <anwer.baccar.pro@gmail.com>
2021-10-11 23:04:23 +0100

    add loading spinner

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,22 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import AboutMeSection from '../../components/Sections/AboutMeSection/AboutMeSection';
-import ContactMeSection from '../../components/Sections/ContactMeSection/ContactMeSection';
+import React, { useEffect, useState } from "react";
+import AboutMeSection from "../../components/Sections/AboutMeSection/AboutMeSection";
+import ContactMeSection from "../../components/Sections/ContactMeSection/ContactMeSection";
 
-import HomeSection from '../../components/Sections/HomeSection/HomeSection';
-import Services from '../../components/Sections/ServicesSection/Services';
-import TechnologiesSection from '../../components/Sections/TechnologiesSection/TechnologiesSection';
-import WorksSection from '../../components/Sections/WorksSection/WorksSection';
-import Layout from '../../hoc/Layout/Layout';
+import HomeSection from "../../components/Sections/HomeSection/HomeSection";
+import Services from "../../components/Sections/ServicesSection/Services";
+import TechnologiesSection from "../../components/Sections/TechnologiesSection/TechnologiesSection";
+import WorksSection from "../../components/Sections/WorksSection/WorksSection";
+import Layout from "../../hoc/Layout/Layout";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
-
-import './Home.css';
+import "./Home.css";
 
 const Home = () => {
   const [responseData, setResponseData] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     fetch(`${process.env.REACT_APP_FIREBASE_URL}/.json`)
       .then((response) => response.json())
       .then((responseData) => {
@@ -26,15 +25,39 @@ const Home = () => {
   }, []);
 
   const rendredComponent = loading ? (
-    <h1 style={{ textAlign: 'center' }}>Loading ...</h1>
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#20a0db6c",
+        position: "absolute",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: "0",
+          right: "0",
+          top: "30%",
+        }}
+      >
+        <Spinner />
+        <h1 style={{ textAlign: "center" }}>Loading...</h1>
+      </div>
+    </div>
   ) : (
     <Layout responseData={responseData.toolBar}>
       <HomeSection responseData={responseData.homeSection} />
-      <AboutMeSection responseData={responseData.aboutSection} cv={responseData.homeSection.cv} />
+      <AboutMeSection
+        responseData={responseData.aboutSection}
+        cv={responseData.homeSection.cv}
+      />
       <Services responseData={responseData.servicesSection} />
       <TechnologiesSection responseData={responseData.technologiesSection} />
       <WorksSection responseData={responseData.worksSection} />
-      <ContactMeSection list={responseData.homeSection.socialNetworkLinks}/>
+      <ContactMeSection list={responseData.homeSection.socialNetworkLinks} />
     </Layout>
   );
 
